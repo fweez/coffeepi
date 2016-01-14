988eb204817741f1e4862b1a195b9d20565819df
Ryan Forsythe <ryan@pdxcb.net>
2016-01-14 15:09:25 -0800

    try printing the temp

--- display.py
+++ display.py
@@ -1,19 +1,33 @@
 #/usr/bin/env python
 
 from SSD1331 import *
+import glob
 
+def currtemp():
+    device_paths = glob.glob('/sys/bus/w1/devices/28*')
+    temps = []
+    for path in device_paths:
+        with open(path + "/w1_slave") as f:
+            lines = f.readlines()
+            if len(lines) != 2:
+                print "Invalid output from", path
+                continue
+            valid = lines[0][-4:] == 'YES\n'
+            if not valid:
+                print path, "does not have a temperature"
+                continue
+            temps.append(float(lines[1][-6:].strip()) / 1000)
+    return temps
+                
 if __name__ == "__main__":
-    raw_balloon  = GetRawPixelDataFromBmp24File("balloon.bmp")
-    data_balloon = UnpackRawPixelBmp24Data(raw_balloon)
-    raw_splash = GetRawPixelDataFromBmp24File("splash.bmp")
-    data_splash = UnpackRawPixelBmp24Data(raw_splash)
     device = SSD1331(SSD1331_PIN_DC, SSD1331_PIN_RST, SSD1331_PIN_CS)
     try:
         while 1:
-            device.DrawFullScreenBitMap(data_balloon)
-            print "drew balloon"
-            device.DrawFullScreenBitMap(data_splash)
-            print "drew splash"
+            temps = currtemp()
+            y = 0
+            for idx, temp in enumerate(temps):
+                device.DrawString(0, y, "Sensor " + str(idx) + ": " + str(temp))
+                y += 10
     finally:
         device.Remove()
 
